93d8a7e4838f7a6f0b5a4f51f64d3e079fc36ee7
lienvdsteen <lienvandensteen@gmail.com>
2015-07-01 18:12:46 +0200

    use gravatar for images

---
+++ config/initializers/gravatarify.rb
@@ -0,0 +1,2 @@
+# set the default image using a Proc
+Gravatarify.options[:default] = Proc.new { |*args| "http://i.dailymail.co.uk/i/pix/2014/08/19/1408456139288_wps_16_A_Bay_Area_man_died_while.jpg" }
