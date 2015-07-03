62cf1e50691a1f1a006ead889d6ad31087b3af4b
Anna Isabel Lamers <isabel.lamers@gmail.com>
2015-07-03 12:17:49 +0200

    uncomment geocode

--- app/models/area.rb
+++ app/models/area.rb
@@ -3,7 +3,7 @@ class Area < ActiveRecord::Base
 
   has_many :sectors
   has_many :pictures
-  geocoded_by :location, if: :latitude.nil?
-  after_validation :geocode, if: Proc.new { |a| a.location_changed? || a.latitude.nil? }
+  # geocoded_by :location, if: :latitude.nil?
+  # after_validation :geocode, if: Proc.new { |a| a.location_changed? || a.latitude.nil? }
   has_many :conversations, :foreign_key => :sender_id
 end
