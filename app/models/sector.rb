4005d4539c4d342cef72f138c7767be08aafef0d
Anna Isabel Lamers <isabel.lamers@gmail.com>
2015-08-20 08:06:30 +0200

    validations for sector added

--- app/models/sector.rb
+++ app/models/sector.rb
@@ -4,4 +4,5 @@ class Sector < ActiveRecord::Base
 
   has_attached_file :document
   validates_attachment :document, :content_type => { :content_type => %w(application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document) }
+  validates :name, :description, :location, :latitude, :longitude, presence: true
 end
