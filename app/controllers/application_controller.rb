0cebed04939fd11eae62cd8bafc7ca510f43e025
Anna Isabel Lamers <isabel.lamers@gmail.com>
2015-09-28 17:12:07 +0200

    redirect to secure protocol added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :redirect_to_https
   before_action :authenticate_user!
+
+  def redirect_to_https
+    redirect_to :protocol => "https://" unless (request.ssl? || request.local?)
+  end
 end
